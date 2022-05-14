8b6442fcda9707cba21e61ea9224c983c388c586
kuramot6f <kuramot6f@icloud.com>
2022-05-14 20:40:18 +0900

    use nextjs

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  exportPathMap: async function () {
+    return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/facilities': { page: '/facilities'},
+        '/location': { page: 'location' },
+        '/apply': { page: '/apply' },
+        '/events': { page: '/events'},
+        '/faq': { page: 'faq' },
+        '/meals': { page: '/meals' },
+        '/messages': { page: '/messages' },
+        '/404': { page: '/404' }, 
+      }
+  },
+  trailingSlash: true,
+  unstable_runtimeJS: true,
+  config: {
+    unstable_runtimeJS: true
+  },
+  images: {
+    loader: "custom"
+  }
+}
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/main.css'
+import '../styles/index.css'
+import '../styles/meals.css'
+import '../styles/facilities.css'
+import '../styles/location.css'
+import '../styles/events.css'
+import '../styles/messages.css'
+import '../styles/about.css'
+import '../styles/faq.css'
+import '../styles/apply.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
